eda5f7fafef2767b11a0d5227ca6e81efce7afec
Ahmet Isleyen <ahmetcny00@gmail.com>
2024-06-09 21:57:44 +0300

    update

--- simpleProjects/codeSpeedMeter/codeSpeedMeter.cpp
+++ simpleProjects/codeSpeedMeter/codeSpeedMeter.cpp
@@ -18,8 +18,19 @@ void fastSum() {
 
 int main() {
 
-	// new commit atýlacak
+	auto start = chrono::high_resolution_clock::now();
+	slowSum();
+	auto end = chrono::high_resolution_clock::now();
+	chrono::duration<double> slowDuration = end - start;
+	double milliseconds1 = slowDuration.count() * 1000;
+	cout << "Slow sum took " << milliseconds1 << " seconds." << endl;
 
+	start = std::chrono::high_resolution_clock::now();
+	fastSum();
+	end = chrono::high_resolution_clock::now();
+	chrono::duration<double> fastDuration = end - start;
+	double milliseconds2 = fastDuration.count() * 1000;
+	std::cout << "Fast sum took " << milliseconds2 << " seconds." << std::endl;
 
 	return 0;
 }
